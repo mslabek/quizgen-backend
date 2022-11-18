a0b634398b3e4597aaa0d6ec4298e9b9a4956393
mslabek <mateusz.slabek@protonmail.com>
2022-11-18 17:33:19 +0100

    docs: add javadoc and swagger docs to controllers

--- src/main/java/com/application/quizgenbackend/item/controller/TopicController.java
+++ src/main/java/com/application/quizgenbackend/item/controller/TopicController.java
@@ -2,6 +2,9 @@ package com.application.quizgenbackend.item.controller;
 
 import com.application.quizgenbackend.item.dto.TopicDto;
 import com.application.quizgenbackend.item.service.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +18,18 @@ public class TopicController {
     private final TopicService topicService;
 
     /**
-     * Returns all topics stored in the database in the enriched dto form. TopicDto contains information about all
-     * property types that belong to the topic, their media type and the amount of distinct properties they refer to.
+     * Retrieves all topics stored in the database. The returned dto contains details about property types that belong
+     * to the topic.
      * <p>
-     * This controller method can be used to further navigate the api. It contains all information needed for
+     * This endpoint can be used to further navigate the api. It contains all information needed for
      * constructing api requests for generating quizzes.
      *
-     * @return list of TopicDtos
+     * @return the list of dtos representing all Topic entities
      */
     @GetMapping("/topics")
+    @Operation(summary = "Retrieves all topics")
+    @ApiResponse(responseCode = "200", description = "Topics retrieved successfully")
+    @ApiResponse(responseCode = "5xx", description = "Unexpected error", content = @Content)
     public List<TopicDto> getTopics() {
         return topicService.getTopics();
     }
--- src/main/java/com/application/quizgenbackend/quiz/controller/QuizController.java
+++ src/main/java/com/application/quizgenbackend/quiz/controller/QuizController.java
@@ -4,8 +4,14 @@ import com.application.quizgenbackend.quiz.dto.QuizDto;
 import com.application.quizgenbackend.quiz.form.AnswerForm;
 import com.application.quizgenbackend.quiz.form.QuizConfigRequest;
 import com.application.quizgenbackend.quiz.mapper.QuizMapper;
+import com.application.quizgenbackend.quiz.model.Question;
+import com.application.quizgenbackend.quiz.model.QuestionResult;
 import com.application.quizgenbackend.quiz.model.Quiz;
+import com.application.quizgenbackend.quiz.model.QuizState;
 import com.application.quizgenbackend.quiz.service.QuizFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,17 +27,46 @@ public class QuizController {
     private final QuizFacade quizFacade;
     private final QuizMapper quizMapper;
 
+    /**
+     * Creates a new {@link Quiz} and bind it to {@link HttpSession session}.
+     *
+     * @param httpSession the session the quiz is to be bound to
+     * @param request     the object specifying data of the quiz
+     * @return the dto representing the generated {@code quiz}
+     */
     @PostMapping("/quiz/new")
-    public QuizDto getQuiz(HttpSession httpSession, @Valid @RequestBody QuizConfigRequest request) {
+    @Operation(summary = "Creates a new quiz")
+    @ApiResponse(responseCode = "200", description = "Quiz created successfully")
+    @ApiResponse(responseCode = "400", description = "Request validation error", content = @Content)
+    @ApiResponse(responseCode = "5xx", description = "Unexpected error", content = @Content)
+    public QuizDto createQuiz(HttpSession httpSession, @Valid @RequestBody QuizConfigRequest request) {
         Quiz quiz = quizFacade.generateQuiz(request);
         httpSession.setAttribute("quiz", quiz);
         return quizMapper.quizToDto(quiz);
     }
 
+    /**
+     * Resolves the answer against the current {@link Quiz} bound to {@link HttpSession session}. The answer is checked
+     * in context of the current {@link Question}. The {@link QuestionResult} of the answer is sent back as a part of
+     * the returned {@code quiz} dto nested in the corresponding {@code question}.
+     * <p>
+     * Every time the answer is received, the {@code quiz dto} updates its state to contain next question. This is
+     * represented in the {@link QuizState} object. If the answer refers to the last question of the {@code quiz}, the
+     * result of the last question is added and the {@code quiz} is marked as solved. From that point, if the controller
+     * receives more answers, it throws an exception.
+     *
+     * @param httpSession the session the quiz is bound to
+     * @param request     the object containing the answer to the current question
+     * @return the dto representing the updated {@code quiz} containing the {@code question result}.
+     */
     @PostMapping("/quiz/answer")
-    public QuizDto postAnswer(HttpSession httpSession, @Valid @RequestBody AnswerForm answerFormDto) {
+    @Operation(summary = "Sends answer to the current question of the current quiz", description = "Resolves the " + "answer against the current Quiz bound to session. The answer is checked in context of the current question. The QuestionResult of the answer is sent back as a part of the returned quiz dto nested in the corresponding question." + "<p>" + "Every time the answer is received, the quiz dto updates its state to contain next question. This is represented in the QuizState object. If the answer refers to the last question of the quiz, the result of the last question is added and the  quiz is marked as solved. From that point, if the controller receives more answers, it throws an exception.")
+    @ApiResponse(responseCode = "200", description = "Answer processed successfully")
+    @ApiResponse(responseCode = "400", description = "Request validation error or quiz already completed", content = @Content)
+    @ApiResponse(responseCode = "5xx", description = "Unexpected error", content = @Content)
+    public QuizDto postAnswer(HttpSession httpSession, @Valid @RequestBody AnswerForm request) {
         Quiz quiz = (Quiz) httpSession.getAttribute("quiz");
-        quizFacade.processAnswer(quiz, answerFormDto);
+        quizFacade.processAnswer(quiz, request);
         return quizMapper.quizToDto(quiz);
     }
 
